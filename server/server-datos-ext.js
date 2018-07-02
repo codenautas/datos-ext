828bec0ce9e4e3d76f2b33533f048ffcbc977139
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-07-02 14:04:51 -0300

    no se versiona lo generado

--- server/server-datos-ext.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_datos_ext_1 = require("./app-datos-ext");
-const operativos_1 = require("operativos");
-const backend_plus_1 = require("backend-plus");
-var AppDatosExt = app_datos_ext_1.emergeAppDatosExt(operativos_1.emergeAppOperativos(backend_plus_1.AppBackend));
-new AppDatosExt().start();
-//# sourceMappingURL=server-datos-ext.js.map
