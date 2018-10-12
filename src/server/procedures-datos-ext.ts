4cf3371a362e240e8362d03be6ff30fbab170b1a
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-10-12 11:36:37 -0300

    Using extracted method; updating field generada from tabla datos

--- src/server/procedures-datos-ext.ts
+++ src/server/procedures-datos-ext.ts
@@ -1,7 +1,6 @@
 "use strict";
 
-import {ProcedureContext, TableDefinition, TableDefinitions, TablaDatos} from "operativos";
-import { AppDatosExtType } from "./app-datos-ext";
+import { AppDatosExtType, ProcedureContext, TableDefinition, TableDefinitions, TablaDatoExterna} from './types-datos-ext'
 
 type TablaDatosGenerarParameters={
     operativo: string
@@ -17,33 +16,33 @@ var procedures = [
         ],
         coreFunction:async function(context:ProcedureContext, parameters:TablaDatosGenerarParameters){
             var be = context.be as AppDatosExtType;
-            let resultTD = await context.client.query(
-                `select *
-                   from tabla_datos, parametros
-                   where operativo = $1 and tabla_datos = $2
-                `,
-                [parameters.operativo, parameters.tabla_datos]
-            ).fetchUniqueRow();
-            if(resultTD.row.estructura_cerrada){
+            let tablaDatos = <TablaDatoExterna> await be.getTablaDatos(context.client, parameters.operativo, parameters.tabla_datos)
+            
+            if(tablaDatos.estructura_cerrada){
                 throw new Error('La tabla ya estaba generada');
             }
-            var tableDef:TableDefinition = await be.generateBaseTableDef(context.client, resultTD.row as TablaDatos); 
+            var tableDef:TableDefinition = await be.generateBaseTableDef(context.client, tablaDatos); 
             var tableDefs: TableDefinitions = {};
             tableDefs[tableDef.name] = be.loadTableDef(tableDef);
+
+            let todayDate = be.getTodayForDB();
+            let updateFechaCalculada = `
+                UPDATE operativos SET calculada='${todayDate}' WHERE operativo='${parameters.operativo}';
+                UPDATE tabla_datos td SET generada='${todayDate}' WHERE td.operativo='${parameters.operativo}' AND td.tabla_datos='${parameters.tabla_datos}';
+            `;
+            let cerrarStructura = `
+                UPDATE tabla_datos
+                SET estructura_cerrada = true
+                WHERE operativo = '${parameters.operativo}' AND tabla_datos = '${parameters.tabla_datos}'
+            `;
             var dump = await be.dumpDbSchemaPartial(tableDefs, {});
             var sqls = [/* 'do $SQL_DUMP$\n begin'*/ ]
             .concat(dump.mainSql)
             .concat(dump.enancePart)
+            .concat(updateFechaCalculada)
+            .concat(cerrarStructura)
             .concat([/* 'end\n$SQL_DUMP$'*/ ]);
             await context.client.query(sqls.join('\n')).execute();
-
-            await context.client.query(
-                `update tabla_datos
-                set estructura_cerrada = true
-                where operativo = $1 and tabla_datos = $2
-                `,
-                [parameters.operativo, parameters.tabla_datos]
-            ).execute();
 
             if(tableDef.primaryKey.length){
                 return 'Listo. Tabla creada con '+tableDef.primaryKey.length+' campos en la pk';
