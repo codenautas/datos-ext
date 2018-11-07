8dc1e15eeb8f7cef70c88a345346605f98f732bb
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-11-07 11:20:05 -0300

    Provisoriamente se le permite al usuario editar e insertar en tabla de datos externa; refactors varios

--- src/server/app-datos-ext.ts
+++ src/server/app-datos-ext.ts
@@ -4,36 +4,29 @@ import * as operativos from "operativos";
 import { TablaDatos } from "operativos";
 import { procedures } from "./procedures-datos-ext";
 
-export * from "operativos";
+export * from "./types-datos-ext";
 
-export type Constructor<T> = new(...args: any[]) => T;
-
-export function emergeAppDatosExt<T extends Constructor<operativos.AppOperativosType>>(Base:T){
+export function emergeAppDatosExt<T extends operativos.Constructor<operativos.AppOperativosType>>(Base:T){
     
     return class AppDatosExt extends Base{
-        myProcedures: operativos.ProcedureDef[] = procedures;
-        myClientFileName: string = 'datos-ext';
         constructor(...args:any[]){ 
             super(args);
-            this.initialize();
+            this.allProcedures = this.allProcedures.concat(procedures);
+            this.allClientFileNames.push({type:'js', module: 'datos-ext', modPath: '../client', file: 'datos-ext.js', path: 'client_modules'})
         }
 
         generateBaseTableDef(tablaDatos:TablaDatos){
             let tDef = super.generateBaseTableDef(tablaDatos);
-            //TODO: dejar de preguntar por el postfix agregar un campo "esCalculada, esExterna o origen" a tablaDatos 
             if (!tablaDatos.esCalculada()){
-                tDef.allow = {...tDef.allow, deleteAll: true, import: true}
+                // TODO: el insert y update true son provisorios para que procesamiento pueda hacer pruebas sin importar todo el excel de datos
+                tDef.allow = {...tDef.allow, deleteAll: true, import: true, insert: true, update: true}
             }
             return tDef
         }
 
         prepareGetTables(){
             super.prepareGetTables();
-            this.getTableDefinition={
-                ...this.getTableDefinition,
-                // origenes,
-                // variables
-            }
+
             this.appendToTableDefinition('parametros', function(tableDef){
                 tableDef.fields.push(
                     {name:'esquema_tablas_externas', typeName:'text', defaultValue:'ext', editable:false}
@@ -41,7 +34,8 @@ export function emergeAppDatosExt<T extends Constructor<operativos.AppOperativos
             });
             this.appendToTableDefinition('tabla_datos', function(tableDef){
                 tableDef.fields.push(
-                    {name:'estructura_cerrada', typeName:'boolean', editable:false}
+                    {name:'estructura_cerrada', typeName:'boolean', editable:false},
+                    {name:"generar"           , typeName:'bigint' , editable:false, clientSide:'generarTD'}
                 );
                 tableDef.constraints.push(
                     {consName:'estructura_cerrada true/null', constraintType:'check', expr:'estructura_cerrada = true'}
