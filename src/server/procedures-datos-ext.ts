8874a64ae4ee519c5b2978ba9cb678dc2f543fec
Raquel Salva <raquelzssalva@gmail.com>
2026-06-11 13:47:30 -0300

    Agrego exclusion de funciones a la llamada a dumDBschemaPartial invocado para generar TD externa. Agrego procedimineto para borrar TDexterna en la base

--- src/server/procedures-datos-ext.ts
+++ src/server/procedures-datos-ext.ts
@@ -29,7 +29,7 @@ var procedures: ProcedureDef[] = [
               WHERE td.operativo=${context.be.db.quoteLiteral(parameters.operativo)} 
               AND td.tabla_datos=${context.be.db.quoteLiteral(parameters.tabla_datos)};`;
   
-            var dump = await be.dumpDbSchemaPartial(tableDefs, {disableDBFunctions:true});
+            var dump = await be.dumpDbSchemaPartial(tableDefs, {disableDBFunctions:true, commonFunsToDisable:['get_app_user-fun.sql','semver_to_decimal-fun.sql']});
             var sqls = [`/* 'do $SQL_DUMP$\n begin'*/ `,dump.mainSql, dump.enancePart, updateFechaCalculada, `/* 'end\n$SQL_DUMP$'*/`];
             await context.client.query(sqls.join('\n')).execute();
 
@@ -38,6 +38,49 @@ var procedures: ProcedureDef[] = [
             }else{
                 throw new Error('ATENCION. Tabla creada sin pk'); 
             }
+        }
+    },   
+    {
+        action:'tabla_datos_fisica_eliminar',
+        parameters:[
+            {name:'operativo'  , typeName:'text', references:'operativos' },
+            {name:'tabla_datos', typeName:'text', references:'tabla_datos'}
+        ],
+        coreFunction:async function(context:ProcedureContext, parameters:CoreFunctionParameters<{ operativo: string; tabla_datos: string }>){
+            let operativoGenerator = new OperativoGenerator(context.client, parameters.operativo);
+            await operativoGenerator.fetchDataFromDB();
+
+            var be = context.be as AppDatosExtType;
+            let tablaDatos = <TablaDatos> (await TablaDatos.fetchOne(context.client, parameters.operativo, parameters.tabla_datos))
+            
+            if(!tablaDatos.generada){
+                throw new Error(`La tabla ${parameters.tabla_datos} no ha sido generada`);
+            }
+            if(tablaDatos.tipo!='externa'){
+                throw new Error(`La tabla ${parameters.tabla_datos} no es una tabla externa`);
+            }
+
+            var tablaExternaFisica= be.db.quoteIdent(tablaDatos.getTableName());
+            var cantReg=(await context.client.query( `
+                select Count(*) from ${tablaExternaFisica}
+            `).fetchUniqueValue()).value;
+            if(cantReg>0){
+                throw new Error(`La tabla ${parameters.tabla_datos} tiene registros, no se puede eliminar`);
+            }
+        
+            var tableDef:TableDefinition = be.generateBaseTableDef(tablaDatos); 
+            delete be.tableStructures[tableDef.name];
+            // falta sacar de la estructura de tablas
+            var dropTablaFisica= `DROP TABLE ${tablaExternaFisica};`; 
+            let updateFechaCalculada = `
+            UPDATE tabla_datos td SET generada=null
+              WHERE td.operativo=${be.db.quoteLiteral(parameters.operativo)} 
+              AND td.tabla_datos=${be.db.quoteLiteral(parameters.tabla_datos)};`;
+  
+            var sqls = [dropTablaFisica, updateFechaCalculada, `/* 'end\n$SQL_DUMP$'*/`];
+            await context.client.query(sqls.join('\n')).execute();
+
+            return 'Listo. Tabla externa '+ parameters.tabla_datos +' eliminada en la Base de Datos ';
         }
     },   
 ];
